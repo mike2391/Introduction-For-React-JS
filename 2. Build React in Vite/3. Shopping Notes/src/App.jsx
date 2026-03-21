a107665c96f884909037d69dd04e2dceb9ea59fd
mike2391 <michael2391valdio@gmail.com>
2026-03-21 21:03:11 +0700

    add shopping notes using Vite + React

--- 2. Build React in Vite/3. Shopping Notes/src/App.jsx
+++ 2. Build React in Vite/3. Shopping Notes/src/App.jsx
@@ -1,51 +1,156 @@
-function App() {
+import { useState } from "react";
+
+const groceryItems = [
+  {
+    id: 1,
+    name: "Kopi Bubuk",
+    quantity: 2,
+    checked: true,
+  },
+  {
+    id: 2,
+    name: "Gula Pasir",
+    quantity: 5,
+    checked: false,
+  },
+  {
+    id: 3,
+    name: "Air Mineral",
+    quantity: 3,
+    checked: false,
+  },
+];
+
+export default function App() {
+  const [items, setItems] = useState(groceryItems);
+
+  function handleAddItem(item) {
+    // common method
+    // setItems([...items, item]);
+
+    // safest method
+    // React fill variable prevItems with previous state value, and then React Add new value [item] and send it back to variable items
+    setItems((prevItems) => [...prevItems, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((prevItems) => prevItems.map((item) => (item.id === id ? { ...item, checked: !item.checked } : item)));
+  }
+
+  function handleClearAll() {
+    setItems([]);
+  }
+
   return (
     <div className="app">
-      <h1>Catatan Belanjaku 📝</h1>
-      <form className="add-form">
-        <h3>Hari ini belanja apa kita?</h3>
-        <div>
-          <select>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-          <input type="text" placeholder="nama barang..." />
-        </div>
-        <button>Tambah</button>
-      </form>
-      <div className="list">
-        <ul>
-          <li>
-            <input type="checkbox" checked="true" />
-            <span style="text-decoration: line-through;">1 Kopi</span>
-            <button>&times;</button>
-          </li>
-          <li>
-            <input type="checkbox" />
-            <span>5 Gula Pasir</span>
-            <button>&times;</button>
-          </li>
-          <li>
-            <input type="checkbox" />
-            <span>3 Air Mineral</span>
-            <button>&times;</button>
-          </li>
-        </ul>
-      </div>
-      <div className="actions">
-        <select>
-          <option value="input">Urutkan berdasarkan urutan input</option>
-          <option value="name">Urutkan berdasarkan nama barang</option>
-          <option value="checked">Urutkan berdasarkan ceklis</option>
-        </select>
-        <button>Bersihkan Daftar</button>
-      </div>
-      <footer className="stats">Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)</footer>
+      <Header />
+      <FormAddItem onAddItem={handleAddItem} />
+      <GroceryList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} onClearAll={handleClearAll} />
+      <Footer />
     </div>
   );
 }
 
-export default App;
+function Header() {
+  return <h1>Catatan Belanjaku 📝</h1>;
+}
+
+function FormAddItem({ onAddItem }) {
+  const [name, setName] = useState("");
+
+  const [qty, setQty] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name) return;
+
+    // no need to declare property value when the value variable have same name with property name
+    const newItem = { name, quantity: qty, checked: false, id: Date.now() };
+    onAddItem(newItem);
+
+    console.log(newItem);
+
+    setName("");
+    setQty(1);
+  }
+
+  const qtyNum = [...Array(20)].map((_, index) => (
+    <option value={index + 1} key={index + 1}>
+      {index + 1}
+    </option>
+  ));
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>Hari ini belanja apa kita?</h3>
+      <div>
+        <select value={qty} onChange={(e) => setQty(Number(e.target.value))}>
+          {qtyNum}
+        </select>
+        <input type="text" placeholder="nama barang..." value={name} onChange={(e) => setName(e.target.value)} />
+      </div>
+      <button>Tambah</button>
+    </form>
+  );
+}
+
+function GroceryList({ items, onDeleteItem, onToggleItem, onClearAll }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  switch (sortBy) {
+    case "name":
+      sortedItems = items.slice().sort((a, b) => a.name.localeCompare(b.name));
+      break;
+
+    case "checked":
+      sortedItems = items.slice().sort((a, b) => a.checked - b.checked);
+      break;
+
+    default:
+      sortedItems = items;
+      break;
+  }
+
+  return (
+    <>
+      <div className="list">
+        <ul>
+          {sortedItems.map((item) => (
+            <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />
+          ))}
+        </ul>
+      </div>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Urutkan berdasarkan urutan input</option>
+          <option value="name">Urutkan berdasarkan nama barang</option>
+          <option value="checked">Urutkan berdasarkan ceklis</option>
+        </select>
+        <button onClick={onClearAll}>Bersihkan Daftar</button>
+      </div>
+    </>
+  );
+}
+
+function Item({ item, onDeleteItem, onToggleItem }) {
+  return (
+    <li key={item.id}>
+      <input type="checkbox" onChange={() => onToggleItem(item.id)} checked={item.checked} />
+      <span style={item.checked ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.name}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>&times;</button>
+    </li>
+  );
+}
+
+function Footer() {
+  return <footer className="stats">Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)</footer>;
+}
