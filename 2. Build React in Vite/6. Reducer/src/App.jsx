9fc4dc4adc993c96bd0eaaaef8196719d7c1b26b
Mike <michael2391valdio@gmail.com>
2026-04-04 12:11:26 +0700

    modify reducer

--- 2. Build React in Vite/6. Reducer/src/App.jsx
+++ 2. Build React in Vite/6. Reducer/src/App.jsx
@@ -1,7 +1,8 @@
 // import { useReducer } from "react";
 import NoteList from "./components/NoteList";
 import NoteForm from "./components/NoteForm";
-import { useImmer } from "use-immer";
+import { useImmerReducer } from "use-immer";
+// import { useReducer } from "react";
 
 let id = 0;
 
@@ -10,31 +11,61 @@ const initialNotes = [
   { id: id++, text: "Learn JavaScript", isEdit: false },
 ];
 
+// function noteReducer(notes, action) {
+//   switch (action.type) {
+//     case "ADD_NOTE":
+//       return [...notes, { id: id++, text: action.text, isDone: false }];
+//     case "CHANGE_NOTE":
+//       return notes.map((note) => {
+//         if (note.id === action.id) {
+//           return { ...note, text: action.text, isDone: action.isDone };
+//         }
+//         return note;
+//       });
+//     case "DELETE_NOTE":
+//       return notes.filter((note) => note.id !== action.id);
+//     default:
+//       return notes;
+//   }
+// }
+
+function noteReducerWithImmer(notes, action) {
+  const index = notes.findIndex((note) => note.id === action.id);
+
+  switch (action.type) {
+    case "ADD_NOTE":
+      notes.push({ id: id++, text: action.text, isDone: false });
+      break;
+    case "CHANGE_NOTE":
+      notes[index].text = action.text;
+      notes[index].isDone = action.isDone;
+      break;
+    case "DELETE_NOTE":
+      notes.splice(index, 1);
+      break;
+    default:
+      return notes;
+  }
+}
+
 export default function NoteApp() {
   // function noteReducer(state, action) {}
 
+  // you can use different name "dispatch" if you want, but best practice is use "dispatch"
   // const [notes, dispatch] = useReducer(noteReducer, initialNotes);
 
-  const [notes, setNotes] = useImmer(initialNotes);
+  const [notes, dispatch] = useImmerReducer(noteReducerWithImmer, initialNotes);
 
   function handleAddNote(text) {
-    setNotes((draft) => {
-      draft.push({ id: id++, text, isEdit: false });
-    });
+    dispatch({ type: "ADD_NOTE", text });
   }
 
   function handleChangeNote(note) {
-    setNotes((draft) => {
-      const index = draft.findIndex((item) => item.id === note.id);
-      draft[index] = note;
-    });
+    dispatch({ type: "CHANGE_NOTE", ...note });
   }
 
   function handleDeleteNote(note) {
-    setNotes((draft) => {
-      const index = draft.findIndex((item) => item.id === note.id);
-      draft.splice(index, 1);
-    });
+    dispatch({ type: "DELETE_NOTE", id: note.id });
   }
 
   return (
